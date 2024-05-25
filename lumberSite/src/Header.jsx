9167b96cfb1f365474c5c8bf3b2c392485431815
Jacob <92279567+Oia20@users.noreply.github.com>
2024-05-25 13:41:00 -0500

    Fixed image not showing on gh pages.

--- lumberSite/src/Header.jsx
+++ lumberSite/src/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
+      <img src='./pexels-fwstudio-33348-172289.jpg'/>
       <div className="header-content">
         <h1>Rob Shaw Lumber</h1>
         <p>Quality lumber for all your building needs</p>

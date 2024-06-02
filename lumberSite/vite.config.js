70a68ce68af48df50b7815da725c32227868b55d
Jacob <92279567+Oia20@users.noreply.github.com>
2024-06-02 11:03:54 -0500

    GH pages dependency

--- lumberSite/vite.config.js
+++ lumberSite/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Simple-Lumber-Site",
+  base: "/Simple-Lumber-Site",
   plugins: [react()],
 })
